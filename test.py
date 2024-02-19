060ded661e106f0b29925de47a0088ac0f200e98
talhatariqbutt <159747083+talhatariqbutt@users.noreply.github.com>
2024-02-19 10:00:59 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,17 @@
-test:
-    pytest test.py
+from main import StudentsInMLOps
+import pytest
+
+def test_enrollStudents():
+    students = StudentsInMLOps()
+    students.enrollStudents(5)
+    assert students.getTotalStrength() == 5
+
+def test_dropStudents():
+    students = StudentsInMLOps()
+    students.enrollStudents(10)
+    students.dropStudents(3)
+    assert students.getTotalStrength() == 7
+
+def test_getClassName():
+    students = StudentsInMLOps()
+    assert students.getClassName() == "StudentsInMLOps"
